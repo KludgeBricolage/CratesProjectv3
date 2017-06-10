1bb393497f803e19d614e712f059d7b6c316b661
KludgeBricolage <KludgeBricolage@github.com>
2017-06-10 21:42:47 +0800

    CratesController comments

--- app/controllers/crates_controller.rb
+++ app/controllers/crates_controller.rb
@@ -1,12 +1,20 @@
 class CratesController < ApplicationController
+
+    # These actions (functions) can be found at the bottom of the controller.
+    # For best practices, these are private actions.
+
     before_action :logged_in_user, only: [:create, :destroy, :edit]
     before_action :correct_crate,   only: [:destroy, :edit, :update]
     before_action :add_profile, only: [:new,:create]
     before_action :check_user_status, only: [:show]
+
+    # Error catching
     rescue_from ::ActiveRecord::RecordNotFound, with: :dont_url_manipulate
     rescue_from ::ActiveRecord::InvalidForeignKey, with: :dont_url_manipulate
 
     def index
+        # Ransack is a search Gem
+        # Pagination done with Kaminari Gem
         @crates = Crate.where(:active_status_id => '1').page(params[:page]).per(12)
         @search = Crate.where(:active_status_id => '1').ransack(params[:q])
         if params.has_key?(:name)
@@ -19,6 +27,8 @@ class CratesController < ApplicationController
     end
 
     def add_profile
+        # User must create a profile first
+        # Comment out the before_action to disable
         if current_user.profile.nil?
             flash[:success] = 'Create a Profile First'
             redirect_to new_user_profile_url(current_user.id)
@@ -26,6 +36,8 @@ class CratesController < ApplicationController
     end
 
     def new
+      # Locations is for the Crate's preferred meet-up location
+      # This is also important for the Mobile application
       @locations = Location.all
       @crate = current_user.crates.build if logged_in?
     end
@@ -34,12 +46,14 @@ class CratesController < ApplicationController
         @crate = current_user.crates.build(crate_params)
         if @crate.save
             if params[:pictures]
-                params[:pictures].each { |image|
-                 @crate.pictures.create(image: image)
-                }
+              # :pictures parameter is an array, check the view's form
+              # Iterate through the pictures to associate it with the crate
+              params[:pictures].each { |image|
+                @crate.pictures.create(image: image)
+              }
             end
-            flash[:success] = 'Crate has been posted!'
-            redirect_to @crate
+              flash[:success] = 'Crate has been posted!'
+              redirect_to @crate
         else
             render new_crate_path
         end
@@ -75,11 +89,16 @@ class CratesController < ApplicationController
     end
 
     def show
+        # This is the only database call
         @crate = friendly_find(params[:id])
+
+        # These instance variables are declared in accordance to the Law of Demeter
         @images = @crate.pictures
         @contact = @crate.user.profile.phone_number
         @query = @crate.queries.build
         @queries = @crate.queries.all
+
+        # Instantiate a new reply
         @reply = Reply.new
     end
 
