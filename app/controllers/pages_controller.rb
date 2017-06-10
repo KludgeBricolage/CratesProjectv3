75b8da3c02ffa27f54b49858839943a8a4a4ae9b
KludgeBricolage <KludgeBricolage@github.com>
2017-06-10 21:34:45 +0800

    PagesController comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
     before_action :is_logged_in, only: [:home,:crate_manager]
-    
+
     def home
+        # Filters
         @acc = search_byst(current_user.id, 1).size or 0
         @iac = search_byst(current_user.id, 2).size or 0
         @fcc = search_byst(current_user.id, 3).size or 0
@@ -10,61 +11,67 @@ class PagesController < ApplicationController
         @q_noti = PublicActivity::Activity.where( trackable: get_c_queries).order("created_at desc").limit(10)
         @f_noti = PublicActivity::Activity.where( trackable: get_f_queries).order("created_at desc").limit(10)
         #get notifications of your forum posts
-    end     
-    
+    end
+
     def crate_manager
+        # Crate status list of selected user
         @active_crates = search_byst(current_user.id, 1)
         @inactive_crates = search_byst(current_user.id, 2)
-        @fin_crates = search_byst(current_user.id, 3)    
+        @fin_crates = search_byst(current_user.id, 3)
     end
-    
+
     def change_status
+        # change Crate status
         Crate.find(params[:crate_id]).update_attributes(active_status_id: params[:active_id])
         redirect_to crate_manager_path
     end
-    
+
     def community
+        # Discussion board
         @users = User.all
-        @groups = Group.all        
+        @groups = Group.all
         @forum_cat = ForumCategory.all
     end
-    
+
     def home_guest
-        
+        # home for guests
     end
-    
-    
+
+
     def get_all_cq
+        # Get all Crate Queries
         @q_noti = PublicActivity::Activity.where( trackable: get_c_queries).order("created_at desc")
     end
-    
+
     def get_all_rep
+        # Get all replies
         @r_noti = PublicActivity::Activity.where( trackable: get_replies).order("created_at desc")
     end
-    
+
     def get_all_fp
+        # Get all Post (forum) Queries
         @f_noti = PublicActivity::Activity.where( trackable: get_f_queries).order("created_at desc")
     end
-    
-    
+
+
     private
     def search_byst(us_id,st_id)
         Crate.where(["user_id = ? and active_status_id = ?", us_id , st_id])
     end
-    
+
     def is_logged_in
         render :home_guest unless logged_in?
     end
-    
+
     def get_c_queries
         c = Query.where(crate: current_user.crates)
     end
-    
+
     def get_replies
         q = Query.where(id: current_user.replies.map {|c| c.query_id}.uniq)
         a = Reply.where(query_id: q).where.not(user: current_user).where.not(
             'created_at < :first_replies',
-            :first_replies => 
+            :first_replies =>
             if Reply.where(query: q, user: current_user).first != nil
                 Reply.where(query: q, user: current_user).first.created_at
             else
@@ -74,12 +81,12 @@ class PagesController < ApplicationController
         b = Reply.where(query: current_user.queries).where.not(user: current_user)
         return a.concat(b)
     end
-    
+
     def get_f_queries
         f = ForumPost.where(id: current_user.forum_comments.map {|c| c.forum_post_id }.uniq)
         a = ForumComment.where(forum_post: f).where.not(user: current_user).where.not(
             'created_at < :first_comments',
-            :first_comments => 
+            :first_comments =>
             if ForumComment.where(forum_post: f, user: current_user).first != nil
                 ForumComment.where(forum_post: f, user: current_user).first.created_at
             else
